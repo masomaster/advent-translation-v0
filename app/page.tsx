65e52cd26e4bb2505f95bdce21034b0516be6c78
masomaster <masonlancaster@protonmail.com>
2023-06-02 10:56:36 -0700

    fixes error due to updating one comp while rendering another

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,16 @@ import SignIn from "./auth/signin/page";
 import SignUp from "./auth/signup/page.jsx";
 import { useAuthContext } from "../context/AuthContext";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
   const { user } = useAuthContext();
   console.log("user", user);
   const router = useRouter();
 
-  if (user) router.push("/dashboard");
+  useEffect(() => {
+    if (user) router.push("/dashboard");
+  }, [user]);
 
   return (
     <>
