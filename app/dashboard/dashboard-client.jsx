4ed8fecd0335429d1204610a2b3f771e118c6b53
masomaster <masonlancaster@protonmail.com>
2023-07-10 15:14:02 -0700

    adds howdy for practice

--- app/dashboard/dashboard-client.jsx
+++ app/dashboard/dashboard-client.jsx
@@ -69,7 +69,7 @@ export default function HomePage({ numOfDays }) {
       </Head>
       <div>
         <h1>
-          Welcome to Advent Translation
+          Howdy! Welcome to Advent Translation
           {profileData.firstName?.length > 0 && `, ${profileData.firstName}`},
           you are on day {profileData.latestDay}!
         </h1>
